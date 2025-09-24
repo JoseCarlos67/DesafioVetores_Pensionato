2c8dd1a3142eef1ecb958b192d94825321bb6794
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-09-23 21:42:37 -0300

    Desafio concluido

---
+++ README.md
@@ -0,0 +1,27 @@
+# 🏨 Exercício – Pensionato com Vetores em Java
+
+![Java](https://img.shields.io/badge/Java-17-orange?logo=java&logoColor=white)
+![Status](https://img.shields.io/badge/Status-Concluído-brightgreen)
+![IDE](https://img.shields.io/badge/IDE-IntelliJ%20IDEA-purple?logo=intellij-idea)
+
+---
+
+## 📌 Enunciado
+A dona de um pensionato possui **10 quartos para alugar a estudantes**, identificados pelos números de **0 a 9**.
+
+O programa deve:
+1. Iniciar com todos os 10 quartos vazios.
+2. Ler um número **N** representando a quantidade de estudantes que vão alugar quartos (**N entre 1 e 10**).
+3. Para cada estudante, ler:
+    - Nome
+    - Email
+    - Número do quarto escolhido (garantindo que esteja vago)
+4. Registrar o aluguel no vetor de quartos.
+5. Ao final, imprimir um **relatório das ocupações**, listando apenas os quartos que estão alugados, em ordem crescente pelo número do quarto.
+
+---
+
+## 🎯 O que se aprende neste exercício
+- Declaração e manipulação de **vetores** em Java.
+- Diferença entre **tipos referência** e **tipos valor**.
+- Manipulação de **entrada de dados** com `Scanner` e `ByteArrayInputStream` para automatizar entradas do teclado.
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import entities.Rent;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.ByteArrayInputStream;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void fillRooms(Rent[] rooms, Scanner scanner) {
+        System.out.print("How many rooms will be rented? ");
+        byte quantity = scanner.nextByte();
+
+        for (byte i = 1; i <= quantity; i++) {
+            System.out.println("\nRent #" + i + ":");
+            System.out.print("Name: ");
+            scanner.nextLine();
+            String name = scanner.nextLine();
+            System.out.print("\nE-mail: ");
+            String email = scanner.next();
+            System.out.print("\nRoom: ");
+            byte roomNumber = scanner.nextByte();
+            System.out.println();
+
+            Rent rent = new Rent(name, email);
+            rooms[roomNumber] = rent;
         }
+    }
+
+    public static void printBusyRooms(Rent[] rooms) {
+        System.out.println("\n\nBusy Rooms: ");
+        for (byte i = 1; i < 10; i++)
+            if (rooms[i] != null)
+                System.out.println(i + ": " + rooms[i]);
+    }
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        String fakeInputString = "3\nMarco Antonio\nmarco@gmail.com\n5\nMaria Green\nmaria@gmail.com\n1\nAlex Grey\nalex@gmail.com\n8";
+        ByteArrayInputStream fakeInput = new ByteArrayInputStream(fakeInputString.getBytes());
+        System.setIn(fakeInput);
+
+        Scanner scanner = new Scanner(System.in);
+
+        Rent[] rooms = new Rent[10];
+        fillRooms(rooms, scanner);
+        printBusyRooms(rooms);
     }
 }
